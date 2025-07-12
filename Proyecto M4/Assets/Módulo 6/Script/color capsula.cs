cc4f556fec6c666c8f45bc1893c1ef69ab536fad
LEITHER117 <emasresmacias@gmail.com>
2025-07-12 15:26:27 -0600

    revisión de scripts

--- Proyecto M4/Assets/Módulo 6/Script/color capsula.cs
+++ Proyecto M4/Assets/Módulo 6/Script/color capsula.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class colorcapsula : MonoBehaviour
+
 {
+    public GameObject capsula;
+    public bool v1;
+    
+    
+    
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
@@ -17,8 +24,31 @@ public class colorcapsula : MonoBehaviour
     }
     private void FixedUpdate ()
     {
-        Color c = new Color(Random.value, Random.value, Random.value);
-        GetComponent<MeshRenderer>().material.color = c;
+        v1 = !v1;
+        if (capsula != null)
+        {
+            Renderer renderer = capsula.GetComponent<Renderer>();
+            if (v1 == true)
+            {
+                renderer.material.color = Color.white;
+            }
+            else
+            {
+                renderer.material.color = Color.black;
+            }
+            switch (v1)
+            {
+                case true:
+                    Debug.Log("blanco");
+                    break;
+                    case false:
+                    Debug.Log("negro");
+                    break;
+            }
+        }
+
+        //Color c = new Color(Random.value, Random.value, Random.value);
+        //GetComponent<MeshRenderer>().material.color = c;
     }
 
 }
